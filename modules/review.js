aef32d5c43e4148f9e8d7e659d65ae5da7773f43
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-05-24 21:18:31 +0200

    add review model

---
+++ modules/review.js
@@ -0,0 +1,21 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let ReviewSchema = new Schema(
+  {
+    game: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
+    sourcePage: { type: String, required: false },
+    content: { type: String, required: false },
+    rating: { type: String, required: true },
+    link: { type: String, required: false },
+  }
+);
+
+ReviewSchema
+.virtual('url')
+.get(function () {
+  return '/review/' + this._id;
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
