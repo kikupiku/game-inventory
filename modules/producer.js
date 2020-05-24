a737c16551334d83c12b5d443dc92979f7b21753
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-05-24 20:48:10 +0200

    add producer schema

--- modules/producer.js
+++ modules/producer.js
@@ -3,5 +3,16 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let ProducerSchema = new Schema(
-  
+  {
+    company: { type: String, required: true },
+    established: { type: Number, required: false, min: 1900, max: 2099 },
+  }
 )
+
+ProducerSchema
+.virtual('url')
+.get(function () {
+  return '/producer/' + this._id;
+});
+
+module.exports = mongoose.model('Producer', ProducerSchema);
